4670a5707e92a861a7ee8acbe3aa7b11bf43384e
EC2 Default User <ec2-user@ip-172-31-35-32.us-east-2.compute.internal>
2020-01-30 02:27:21 +0000

    HW1 Finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,97 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    ans = 0;
+    arr.each { |num|
+        ans += num;
+    }
+    return ans;
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr.size == 0 
+        return 0
+    elsif arr.size == 1
+        return arr[0]
+    else
+        max = -Float::INFINITY
+        secMax = -Float::INFINITY
+        arr.each { |num|
+            if num > max
+                secMax = max
+                max = num
+            elsif num > secMax
+                secMax = num
+            end
+        }
+        return max + secMax
+    end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    if arr.size <= 1
+        return false
+    end
+    lookup = {}
+    arr.each { |num|
+        if lookup.has_key?(n - num)
+            return true
+        end
+        if !lookup.has_key?(num)
+            lookup[num] = true
+        end
+    }
+    return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+    return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    s.downcase =~ /\A[b-df-hj-np-tv-z]/
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	return s !~ /\D/ && s.size != 0 && s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	def initialize(isbn, price)
+		raise ArgumentError.new(
+    		"ISBN number should not be empty!"
+  		) if isbn.size == 0
+
+  		raise ArgumentError.new(
+    		"Price should not less than or equal to zero!"
+  		) if price <= 0
+
+      	@isbn=isbn
+      	@price=price
+   	end
+
+   	def isbn=(isbn)
+    	@isbn = isbn
+  	end
+
+  	def isbn
+    	@isbn
+  	end
+
+  	def price=(price)
+    	@price = price
+  	end
+
+  	def price
+    	@price
+  	end
+
+  	def price_as_string
+  		return "$%0.2f" % [@price]
+  	end
+
 end
